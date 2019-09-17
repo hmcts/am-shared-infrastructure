370898e3eb8a301ddc6745febfec79bb7ed11845
Benjamin Matthews <benjamin.matthews@hmcts.net>
2019-09-17 13:21:31 +0100

    add managed identity object id for all envs

--- key-vault.tf
+++ key-vault.tf
@@ -7,4 +7,5 @@ module "vault" {
   object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "${var.product_group_object_id}"
+  managed_identity_object_id = "${var.managed_identity_object_id}"
 }
---
+++ perftest.tfvars
@@ -0,0 +1,1 @@
+managed_identity_object_id="a22fd6c0-9004-4d15-8e75-7d36638d817e"
