31441eea427e6c0a0eed65e33bab811fb23b7da0
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-09-21 14:51:24 +0100

    Removed terraform legacy provider

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  alias = "legacy"
-  version = "=1.44.0"
-}
-
 terraform {
   required_version = ">= 0.13"  # Terraform client version
 
