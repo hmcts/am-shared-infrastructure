a64a15233b3cd266e9f7177394d77713be0bd3e6
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-12-04 10:30:16 +0000

    Add branc sync script

--- key-vault.tf
+++ key-vault.tf
@@ -1,14 +1,14 @@
 module "vault" {
-  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                        = join("-", [var.product, var.env])
-  product                     = join("-", [var.product, "shared"])
-  env                         = var.env
-  tenant_id                   = var.tenant_id
-  object_id                   = var.jenkins_AAD_objectId
-  resource_group_name         = azurerm_resource_group.rg.name
-  product_group_object_id     = var.product_group_object_id
-  create_managed_identity     = true
-  common_tags                 = local.common_tags
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = join("-", [var.product, var.env])
+  product                 = join("-", [var.product, "shared"])
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_object_id = var.product_group_object_id
+  create_managed_identity = true
+  common_tags             = local.common_tags
 }
 
 resource "azurerm_key_vault_secret" "appInsightsInstrumentationKey" {
