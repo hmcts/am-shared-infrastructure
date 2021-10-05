4126e0b821fca4577ed572079fab6da90a17b186
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-10-05 20:12:54 +0100

    Upgrade TF and AzureRM version, remove unwanted vars

--- terraform.tf
+++ terraform.tf
@@ -9,14 +9,13 @@ provider "azurerm" {
 }
 
 terraform {
-  required_version = ">= 0.13" # Terraform client version
 
   backend "azurerm" {}
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25" # AzureRM provider version
+      version = "~> 2.78.0" # AzureRM provider version
     }
 
     azuread = {
