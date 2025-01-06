d4c3d3fb2141e49db73ee17a862d6bf5843df095
Sammi Chong <shinyee.chong@hmcts.net>
2025-01-06 13:02:27 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0.0" # AzureRM provider version
+      version = "~> 4.0" # AzureRM provider version
     }
 
     azuread = {
